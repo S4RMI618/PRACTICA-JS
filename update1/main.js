84ca9a7f5f24a752050791456176ace0b2cb9160
S4RMI618 <huessabe@gmail.com>
2022-11-01 14:51:19 -0500

    Orden

--- update1/main.js
+++
@@ -1,8 +0,0 @@
-/* window.addEventListener("load", ejecutarOpcion) */
-
-document.getElementById('opciones'.addEventListener('change', ejecutarOpcion))
-
-function ejecutarOpcion() {
-    let opcion = document.getElementById('opciones');
-    alert('Holaaaa...' + opcion);
-}
